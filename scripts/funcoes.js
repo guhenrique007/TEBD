851aa52a79af92f42ad93f41078b18f8104a086d
Gustavo Henrique Lima <drgustav1997@gmail.com>
2018-07-11 12:41:48 -0300

    Add files via upload
    final

--- scripts/funcoes.js
+++ scripts/funcoes.js
@@ -1,3 +1,7 @@
+var indexFilme;
+var vetorFilmeId= [];
+
+
 $(document).ready(function() {
   var queryString =
   'query=PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> '+
@@ -11,18 +15,20 @@ $(document).ready(function() {
   
 });
 
-var indexFilme;
+ 
 
 $(document).ready(function() {
 //funcao que escolhe o filme e pede pra listar atores do filme x
   $('select.lista-filmes').change(function() {
     var nomeDoFilme = $('select.lista-filmes').val();
-    indexFilme = $('select.lista-filmes').index('option');
-    console.log(indexFilme);
-    indexFilme = indexFilme - 1;
-    
-    var idFilme = retornaIdFilme(indexFilme);
+    var filmeId;
+    vetorFilmeId.forEach(element => {
+      if (element.nome == nomeDoFilme){
+          filmeId = element.id;
+      }            
+    });
 
+   // console.log(filmeId);
 	
 	/*
 	 * FILME TÀ SENDO PASSADO NA MÂO
@@ -33,15 +39,19 @@ $(document).ready(function() {
       'SELECT DISTINCT ?subject ?predicate ?object '+
       'WHERE { '+
       '?subject ?predicate ?object '+
-      'FILTER(str(?subject) = "http://movieland.com/ufrrj/tebd/#movie/187" ) '+
+      'FILTER(str(?subject) = "http://movieland.com/ufrrj/tebd/#movie/'+filmeId +'" ) '+
       '} ';
+
+/*
+   * FILME TÀ SENDO PASSADO NA MÂO
+   * */
 
     var queryStringAtores =
       'query=PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> '+
       'PREFIX j.0: <http://movieland.com/ufrrj/tebd/#> '+
       'SELECT DISTINCT ?subject ?predicate ?object '+
       'WHERE { '+
-        '?subject j.0:movieId "187" . '+
+        '?subject j.0:movieId "'+filmeId +'" . '+
         '?subject j.0:actorId ?object '+
     '} ';
 
@@ -52,30 +62,6 @@ $(document).ready(function() {
 
 });
 
-function retornaIdFilme(indexFilme){
-  
-  var queryString =
-  'query=PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> '+
-        'PREFIX j.0: <http://movieland.com/ufrrj/tebd/#> '+
-        'SELECT DISTINCT ?subject ?object '+
-        'WHERE { ' +
-        '?subject ?predicate ?object ' +
-        'FILTER(str(?predicate) = "http://movieland.com/ufrrj/tebd/#movieId") '+
-        '} ';
-   
-  fetch('http://localhost:3030/filmes/sparql', {
-          method: 'post',
-          headers: new Headers({'content-type': 'application/x-www-form-urlencoded'}),
-          body: queryString
-        }).then(function(response) {
-          return response.json();
-          })
-          .then(function (json) {
-	      console.log(indexFilme);
-	      console.log(json.results.bindings[indexFilme]);
-              return json.results.bindings[indexFilme];
-          })
-}
 
 function listaFilmesAjax(queryString) {
   fetch('http://localhost:3030/filmes/sparql', {
@@ -89,6 +75,11 @@ function listaFilmesAjax(queryString) {
               json.results.bindings.forEach(element => {
                 $('select.lista-filmes').append(
                     '<option>' + element.object.value + '</option>');
+                  
+                  var nomeFilmeId = {"nome":"","id":""};
+                  nomeFilmeId.nome = element.object.value;
+                  nomeFilmeId.id = element.subject.value.replace( /^\D+/g, '');
+                  vetorFilmeId.push(nomeFilmeId);
               })
           })
 }
@@ -96,6 +87,24 @@ function listaFilmesAjax(queryString) {
 //já que eu n posso testar muito vou repetir codigo
 function printaFilmeJson(queryString){
   
+   var atributos = [
+    'title ',
+    'descrição ',
+    'id linguagem ',
+    'rental duration ',
+    'replacemente Cost',
+    'movieId ',
+    'release year ',
+    'lenght ',
+    'lastUpdate ',
+    'rating ',
+    'rental rating ',
+    'full text ',
+    'special features'
+   ]
+
+   var indAtrib = 0;
+
    fetch('http://localhost:3030/filmes/sparql', {
           method: 'post',
           headers: new Headers({'content-type': 'application/x-www-form-urlencoded'}),
@@ -107,7 +116,8 @@ function printaFilmeJson(queryString){
               //console.log(json.results.bindings);
               json.results.bindings.forEach(element => {
 	        var node = document.createElement("LI");
-	        var textnode = document.createTextNode(element.object.value + " \n");
+          //console.log(element.object);
+          var textnode = document.createTextNode(element.object.value + " \n");
 	        node.appendChild(textnode);
 	        document.getElementsByClassName("atores")[0].appendChild(node);
               })
@@ -124,7 +134,7 @@ function printaAtores(queryString){
           return response.json();
           })
           .then(function (json) {
-              console.log(json.results.bindings);
+             // console.log(json.results.bindings);
               json.results.bindings.forEach(element => {
 		/*console.log('to aqui');
 	        console.log(element.object.value);*/
@@ -154,13 +164,103 @@ function printaAtor(atorId){
           })
           .then(function (json) {
               //console.log(json.results.bindings);
-              json.results.bindings.forEach(element => {
+          var jaEntrou = false;
+          json.results.bindings.forEach(element => {
 
-	        var node = document.createElement("LI");
-	        var textnode = document.createTextNode(element.object.value + " \n");
+	        var node = document.createElement("a");
+          node.setAttribute('href','#');
+          node.id = atorId;
+          node.onclick = printaId;
+	        var textnode = document.createTextNode(element.subject.value.replace( /^\D+/g, '') + " \n");
 	        node.appendChild(textnode);
-	        document.getElementsByClassName("atores")[0].appendChild(node);
+	        if(!jaEntrou){
+            document.getElementsByClassName("idAtores")[0].appendChild(node);  
+            jaEntrou = true;
+          }
+          
+              
               })
           })
 }
 
+function printaId(event){
+ var idAtor = event.target.id;
+var queryStringAtores =
+      'query=PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> '+
+      'PREFIX j.0: <http://movieland.com/ufrrj/tebd/#> '+
+      'SELECT DISTINCT ?subject ?predicate ?object '+
+      'WHERE { '+
+        '?subject j.0:actorId "'+idAtor+'" . '+
+        '?subject j.0:movieId ?object '+
+    '} ';
+
+    fetch('http://localhost:3030/actors_movies/sparql', {
+          method: 'post',
+          headers: new Headers({'content-type': 'application/x-www-form-urlencoded'}),
+          body: queryStringAtores
+
+        }).then(function(response) {
+          return response.json();
+          })
+          .then(function (json) {
+              //console.log(json.results.bindings);
+              document.write('IDS DOS FILMES QUE O ATOR ATUOU:'+ '<br>' +'<br>');
+              json.results.bindings.forEach(element => {
+               // console.log(element.object.value);
+                var nomeDoAtor;
+              
+                var info = element.object.value + '<br>';
+                var infoCompleta = info;
+                document.write(infoCompleta);
+
+              })
+              printaAtorCompleto(idAtor);
+          })
+
+}
+
+function printaAtorCompleto(idAtor){
+  var queryString= 
+   'query=PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> '+
+   'PREFIX j.0: <http://movieland.com/ufrrj/tebd/#> '+
+   'SELECT DISTINCT ?subject ?predicate ?object '+
+   'WHERE { '+
+   '?subject ?predicate ?object '+
+   'FILTER(str(?subject) = "http://movieland.com/ufrrj/tebd/#actor/'+idAtor+'" ) '+
+   '} ';
+
+   fetch('http://localhost:3030/actors/sparql', {
+          method: 'post',
+          headers: new Headers({'content-type': 'application/x-www-form-urlencoded'}),
+          body: queryString
+        }).then(function(response) {
+          return response.json();
+          })
+          .then(function (json) {
+              //console.log(json.results.bindings);
+          var jaEntrou = false;
+          document.write('<br>'+'NOME DO ATOR:'+ '<br>' +'<br>');
+          json.results.bindings.forEach(element => {
+
+          var node = document.createElement("a");
+          node.setAttribute('href','#');
+          node.id = idAtor;
+          node.onclick = printaId;
+          var textnode = document.createTextNode(element.object.value + " \n");
+          //console.log(textnode);
+          node.appendChild(textnode);
+          if(!jaEntrou){
+            //document.getElementsByClassName("atores")[0].appendChild(node);
+            document.write(element.object.value);  
+            //jaEntrou = true;
+          }
+          
+              
+              })
+          })
+
+
+
+
+
+}
